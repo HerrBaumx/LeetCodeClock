4b2bc572e12cddec5b761c1621a80cd7c1fd220b
HerrBaumx <731278859@qq.com>
2020-06-18 19:06:15 +0800

    1028. 从先序遍历还原二叉树 迭代

---
+++ src/com/jxiao/june20/RecoverFromPreorder1028.java
@@ -0,0 +1,90 @@
+package com.jxiao.june20;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 我们从二叉树的根节点 root 开始进行深度优先搜索。
+ * <p>
+ * 在遍历中的每个节点处，我们输出 D 条短划线（其中 D 是该节点的深度），然后输出该节点的值。（如果节点的深度为 D，则其直接子节点的深度为 D + 1。根节点的深度为 0）。
+ * <p>
+ * 如果节点只有一个子节点，那么保证该子节点为左子节点。
+ * <p>
+ * 给出遍历输出 S，还原树并返回其根节点 root。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入："1-2--3--4-5--6--7"
+ * 输出：[1,2,5,3,4,6,7]
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入："1-2--3---4-5--6---7"
+ * 输出：[1,2,5,3,null,6,null,4,null,7]
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入："1-401--349---90--88"
+ * 输出：[1,401,null,349,88,90]
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 原始树中的节点数介于 1 和 1000 之间。
+ * 每个节点的值介于 1 和 10 ^ 9 之间。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/recover-a-tree-from-preorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RecoverFromPreorder1028 {
+
+    public TreeNode recoverFromPreorder(String S) {
+
+        Deque<TreeNode> path = new LinkedList<TreeNode>();
+        int pos = 0;
+        while (pos < S.length()) {
+            int level = 0;
+            while (S.charAt(pos) == '-') {
+                level++;
+                pos++;
+            }
+            int value = 0;
+            while (pos < S.length() && Character.isDigit(S.charAt(pos))) {
+                value = value * 10 + (S.charAt(pos) - '0');
+                pos++;
+            }
+            TreeNode node = new TreeNode(value);
+            if (level == path.size()) {
+                if (!path.isEmpty()) {
+
+                    path.peek().left = node;
+                }
+            }else {
+                while (level != path.size()) {
+                    path.pop();
+                }
+                path.peek().right = node;
+            }
+            path.push(node);
+        }
+        while (path.size() > 1) {
+            path.pop();
+        }
+        return path.peek();
+
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
