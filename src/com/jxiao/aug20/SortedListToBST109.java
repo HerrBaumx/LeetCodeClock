620b52df6a4168d4fe81bf53cc24c82be8c19fa0
HerrBaumx <731278859@qq.com>
2020-08-18 08:07:09 +0800

    109. 有序链表转换二叉搜索树 分治

---
+++ src/com/jxiao/aug20/SortedListToBST109.java
@@ -0,0 +1,88 @@
+package com.jxiao.aug20;
+
+/**
+ * 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
+ * <p>
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定的有序链表： [-10, -3, 0, 5, 9],
+ * <p>
+ * 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
+ * <p>
+ * 0
+ * / \
+ * -3   9
+ * /   /
+ * -10  5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/convert-sorted-list-to-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SortedListToBST109 {
+    public TreeNode sortedListToBST(ListNode head) {
+
+        return buildTree(head, null);
+
+    }
+
+    private TreeNode buildTree(ListNode left, ListNode right) {
+        if (left == right) {
+            return null;
+        }
+        ListNode mid = getMedian(left, right);
+        TreeNode root = new TreeNode(mid.val);
+        root.left = buildTree(left, mid);
+        root.right = buildTree(mid.next, right);
+        return root;
+    }
+
+    private ListNode getMedian(ListNode left, ListNode right) {
+
+        ListNode fast = left;
+        ListNode slow = left;
+        while (fast != right && fast.next != right) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
